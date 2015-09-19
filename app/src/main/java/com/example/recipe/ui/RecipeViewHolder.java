a3138cacfcd44e1496dc4f138131de35d1e2f612
Anuradha <anuradhachatterjee188@gmail.com>
2015-09-19 23:14:19 +0530

    image download and next button

--- app/src/main/java/com/example/recipe/ui/RecipeViewHolder.java
+++ app/src/main/java/com/example/recipe/ui/RecipeViewHolder.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -23,33 +24,50 @@ public class RecipeViewHolder extends RecyclerView.ViewHolder {
     private View mContent;
     protected ImageView mIcon;
     protected TextView mTitle;
-    RecipeViewHolderListener listener;
+    RecipeViewHolderListener mListener;
 
     public RecipeViewHolder(View view, final RecipeViewHolderListener lstr) {
         super(view);
         mIcon = (ImageView) view.findViewById(R.id.icon);
         mTitle = (TextView) view.findViewById(R.id.firstLine);
         mContent = view.findViewById(R.id.icon);
-        listener = lstr;
+        mListener = lstr;
 
         ViewGroup.LayoutParams layoutParams = mContent.getLayoutParams();
         layoutParams.height = (int) (Config.SCREEN_SIZE.y
                 * Config.MAX_CATEGORY_CARD_HEIGHT_PECENTAGE);
         mContent.setLayoutParams(layoutParams);
         mContent.requestLayout();
+        Button downloadButton = (Button) view.findViewById(R.id.download);
+        Button nextButton = (Button) view.findViewById(R.id.next);
+
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
 
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (listener != null) {
+                if (mListener != null) {
                     String description = "this is the most yummlicious recipe";
                     RecipeDescription recipeDescription = RecipeDescription.getRecipeDescription();
-                    listener.onViewHolderClicked(recipeDescription);
+                    mListener.onViewHolderClicked(recipeDescription);
                     Log.d("TAG","in view holder click" +description);
 
                 }
             }
         });
     }
-
 }
