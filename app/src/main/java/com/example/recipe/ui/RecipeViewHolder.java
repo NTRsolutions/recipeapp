aadcaae7bad2c49d819f302a47d5af5384b8a81a
Anuradha <anuradhachatterjee188@gmail.com>
2015-09-29 19:11:03 +0530

    recipe holder

--- app/src/main/java/com/example/recipe/ui/RecipeViewHolder.java
+++ app/src/main/java/com/example/recipe/ui/RecipeViewHolder.java
@@ -10,6 +10,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
